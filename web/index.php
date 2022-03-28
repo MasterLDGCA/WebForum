0f45115b2398cc40e0dc763b7949f7c5894fc92f
Charith <charithakalanka@live.com>
2022-03-29 02:19:54 +1100

    DB test

--- web/index.php
+++ web/index.php
@@ -4,9 +4,21 @@ define( 'PAGE_TITLE', 'Home');
 
 require 'inc/header.inc.php';
 require 'inc/postgresql.inc.php';
-
 ?>
 
 <div class="title">
   <h1>Welcome to WebForum</h1>
 </div>
+
+<?php
+  $stmt = "SELECT * FROM \"Users\"";
+  $result = pg_query($db_connection, $stmt);
+
+  while ($row = pg_fetch_row($result)) {
+    print_r($row);
+  }
+?>
+
+<script>
+    console.log(<?= json_encode($result); ?>);
+</script>

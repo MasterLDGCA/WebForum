2fc9ee2064cebe061b0037795e1616abf55b99fc
Charith <charithakalanka@live.com>
2022-03-29 03:19:53 +1100

    Format post view - retrieve comments

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@ require 'inc/postgresql.inc.php';
 </div>
 
 <?php
-  $stmt = 'select p.title , p."content", u.first_name , u.last_name, p.created_at  from "Posts" p
+  $stmt = 'select p.title , p."content", u.first_name , u.last_name, p.created_at, p.id  from "Posts" p
             left join "Users" u ON p.user_id = u.id
             where p.visible = true and p.approved = true';
   $result = pg_query($db_connection, $stmt);
@@ -20,11 +20,25 @@ require 'inc/postgresql.inc.php';
     echo "<div class=\"post_node\">";
     echo "<div class=\"post_title\">".$row[0]."</div>";
     echo "<div class=\"post_content\">".$row[1]."</div>";
-    echo "<div>";
+    echo "<div class=\"post_footer\">";
     echo "<div class=\"post_author\">".$row[2]." ".$row[3]."</div>";
     echo "<div class=\"post_date\">".$row[4]."</div>";
     echo "</div>";
+    ;
+
+    // Retrieve comments
+    $stmt = 'select u.first_name , u.last_name , c."comment" , c.created_at from "Comments" c
+              left join "Users" u on c.user_id = u.id
+              where c.visible = true and c.approved = true and c.post_id = '.$row[5];
+
+    $result = pg_query($db_connection, $stmt);
+
+    while ($row = pg_fetch_row($result)) {
+      print_r($row);
+    }
+
     echo "</div>";
-    echo "<hr>";
+    echo "<hr>"
+
   }
 ?>

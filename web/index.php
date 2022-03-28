b865dba1db2283f1f3fccffcff200426a98f6aa7
Charith <charithakalanka@live.com>
2022-03-29 03:31:17 +1100

    Backtrack

--- web/index.php
+++ web/index.php
@@ -9,23 +9,3 @@ require 'inc/postgresql.inc.php';
 <div class="title">
   <h1>Welcome to WebForum</h1>
 </div>
-
-<?php
-  $stmt = 'select p.title , p."content", u.first_name , u.last_name, p.created_at from "Posts" p
-            left join "Users" u ON p.user_id = u.id
-            where p.visible = true and p.approved = true';
-  $result = pg_query($db_connection, $stmt);
-
-  while ($row = pg_fetch_row($result)) {
-    echo "<div class=\"post_node\">";
-    echo "<div class=\"post_title\">".$row[0]."</div>";
-    echo "<div class=\"post_content\">".$row[1]."</div>";
-    echo "<div class=\"post_footer\">";
-    echo "<div class=\"post_author\">".$row[2]." ".$row[3]."</div>";
-    echo "<div class=\"post_date\">".$row[4]."</div>";
-    echo "</div>";
-    echo "</div>";
-    echo "<hr>"
-
-  }
-?>

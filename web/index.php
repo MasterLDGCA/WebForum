d6dd6a3459bf24f45b07c10a41cd01df74c0fb2f
Charith <charithakalanka@live.com>
2022-03-29 02:34:51 +1100

    DB test

--- web/index.php
+++ web/index.php
@@ -11,12 +11,27 @@ require 'inc/postgresql.inc.php';
 </div>
 
 <?php
-  $stmt = 'select p.title , p."content", u.first_name , u.last_name, p.created_at  from "Posts" p  
+  $stmt = 'select p.title , p."content", u.first_name , u.last_name, p.created_at  from "Posts" p
             left join "Users" u ON p.user_id = u.id
             where p.visible = true and p.approved = true';
   $result = pg_query($db_connection, $stmt);
 
   while ($row = pg_fetch_row($result)) {
-    print_r($row);
+?>
+<div class="post_title">
+  <?echo $row[0]?>
+</div>
+<div class="post_body">
+  <?echo $row[1]?>
+</div>
+<div class="post_author">
+  <?echo $row[2]." "$row[3]?>
+</div>
+<div class="post_date">
+  <?echo $row[4]?>
+</div>
+<hr />
+
+<?php
   }
 ?>

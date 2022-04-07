9c642014361e66e98b00f9c3b45ca654a9055c32
Charith <charithakalanka@live.com>
2022-04-07 17:21:56 +1000

    script for DB tests

--- web/db_test.php
+++ web/db_test.php
@@ -2,8 +2,10 @@
 
 require 'inc/postgresql.inc.php';
 
-$pass = 'test';
-
-print_r(password_hash($pass, PASSWORD_DEFAULT));
+$username = 'test1@webforum.com';
+$stmt = "select first_name, last_name, pass_hash, is_admin from \"Users\" u where email = '".$username."'";
+$check = pg_query($db_connection, $stmt);
+$result = pg_fetch_row($check);
+print_r($result);
 
 ?>

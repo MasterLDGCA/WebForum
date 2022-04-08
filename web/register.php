4d9c62a6135ad732242a7e35db8a9e671b439f25
Charith <charithakalanka@live.com>
2022-04-08 20:18:20 +1000

    Working registration page

--- web/register.php
+++ web/register.php
@@ -3,32 +3,47 @@ define( 'PAGE', 'Register' );
 define( 'PAGE_TITLE', 'Register');
 
 require 'inc/header.inc.php';
+require 'inc/functions.inc.php';
 
 if ($loggedIn) {
   header( 'location: /profile.php' );
   exit();
 }
 
+$errors = null;
+$errors = new_user_validation();
+
+if (!empty($_POST["fname"]) && !empty($_POST["lname"]) && !empty($_POST["email"]) && !empty($_POST["password"]) && !$errors) {
+  // Add user to the database
+  insert_user($db_connection,$_POST["fname"],$_POST["lname"],$_POST["email"],$_POST["password"]);
+}
 
 ?>
 <div class="content">
   <div class="form-box">
+    <?php
+      if ($errors) {
+        foreach($errors as $error) {
+          echo "<div class=\"error_msg\">*".$error."</div>\n";
+        }
+      }
+    ?>
     <h1>Register for a new account</h1>
     <form method="POST" action="register.php">
       <div class="form-group">
-          <input type="fname" class="form-control" name="fname" placeholder="Your First Name" value="<?=$email;?>">
+          <input type="text" class="form-control" name="fname" placeholder="Your First Name" value="<?=$_POST["fname"];?>">
       </div>
       <div class="form-group">
-          <input type="lname" class="form-control" name="lname" placeholder="Your Last Name" value="<?=$email;?>">
+          <input type="text" class="form-control" name="lname" placeholder="Your Last Name" value="<?=$_POST["lname"];?>">
       </div>
       <div class="form-group">
-          <input type="email" class="form-control" name="email" placeholder="Your Email" value="<?=$email;?>">
+          <input type="email" class="form-control" name="email" placeholder="Your Email" value="<?=$_POST["email"];?>">
       </div>
       <div class="form-group">
           <input type="password" class="form-control" name="password" placeholder="Your Password">
       </div>
       <div class="form-group">
-          <input type="password2" class="form-control" name="password2" placeholder="Re-enter your password" value="<?=$email;?>">
+          <input type="password" class="form-control" name="password2" placeholder="Re-enter your password" value="<?=$email;?>">
       </div>
       <button type="submit" class="btn btn-secondary">Register</button>
     </form>

9d17d114aa0eb58b23410d028e0d5f64c48cc917
Charith <charithakalanka@live.com>
2022-04-17 13:54:06 +1000

    CreatePost button update

--- web/inc/header.inc.php
+++ web/inc/header.inc.php
@@ -32,7 +32,7 @@ require 'inc/session.inc.php';
           <?php
           if ($isAdmin) echo "<li class=\"nav-item\"><a href=\"/admin.php\">Administration</a></li>\n";
 
-          if ($loggedIn) echo "<li class=\"nav-item\"><a href=\"/formpost.php\">Create Post</a></li>\n";
+          echo "<li class=\"nav-item\"><a href=\"/formpost.php\">Create Post</a></li>\n";
 
           if ($loggedIn) echo "<li class=\"nav-item\"><a href=\"/profile.php\">Profile</a></li>\n";
           else echo "<li class=\"nav-item\"><a href=\"/register.php\">Register</a></li>\n";

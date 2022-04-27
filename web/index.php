f900ee80cda7481ee8cc02d662a8fb7b96f86c50
Charith <charithakalanka@live.com>
2022-04-27 20:03:56 +1000

    Fix leaderboard order

--- web/index.php
+++ web/index.php
@@ -140,7 +140,7 @@ if (!empty($_POST["comment_like"])) {
             <th>Score</th>
           </tr>
           <?php
-            $stmt = ' select concat(u2.first_name,\' \',u2.last_name) , post_likes.author, post_likes.post_likes , comment_likes.comment_likes
+            $stmt = ' select concat(u2.first_name,\' \',u2.last_name) , post_likes.author, post_likes.post_likes , comment_likes.comment_likes , coalesce(post_likes.post_likes, 0) + coalesce(comment_likes.comment_likes, 0) as score
                       from (select u.id as author , sum(post_likes.likes) as post_likes
                       		from "Users" u
                       		left join "Posts" p on p.user_id = u.id
@@ -152,19 +152,19 @@ if (!empty($_POST["comment_like"])) {
                       		left join (select cl.comment_id, count(*) as likes from "CommentLikes" cl where cl.created_at >\''.date('Y-m-d', strtotime('-1 week')).'\' group by cl.comment_id) as comment_likes on comment_likes.comment_id = c.id
                       		group by author) as comment_likes
                       on post_likes.author = comment_likes.author
-                      left join "Users" u2 on u2.id = post_likes.author';
+                      left join "Users" u2 on u2.id = post_likes.author
+                      order by score desc';
 
             $alltime_leaders = pg_query($db_connection, $stmt);
 
             while ($alltime_leader_row = pg_fetch_row($alltime_leaders)) {
               // print_r($alltime_leader_row);
-              $final_score = $alltime_leader_row[2] + $alltime_leader_row[3];
-              if (!$final_score) continue;
+              if ($alltime_leader_row[4]<1) continue;
               echo "<tr>";
               echo "<td>".$alltime_leader_row[0]."</td>";
               echo "<td>".$alltime_leader_row[2]."</td>";
               echo "<td>".$alltime_leader_row[3]."</td>";
-              echo "<td>".$final_score."</td>";
+              echo "<td>".$alltime_leader_row[4]."</td>";
               echo "</tr>";
             }
 
@@ -184,7 +184,7 @@ if (!empty($_POST["comment_like"])) {
             <th>Score</th>
           </tr>
           <?php
-            $stmt = ' select concat(u2.first_name,\' \',u2.last_name) , post_likes.author, post_likes.post_likes , comment_likes.comment_likes
+            $stmt = ' select concat(u2.first_name,\' \',u2.last_name) , post_likes.author, post_likes.post_likes , comment_likes.comment_likes , coalesce(post_likes.post_likes, 0) + coalesce(comment_likes.comment_likes, 0) as score
                       from (select u.id as author , sum(post_likes.likes) as post_likes
                       		from "Users" u
                       		left join "Posts" p on p.user_id = u.id
@@ -196,19 +196,19 @@ if (!empty($_POST["comment_like"])) {
                       		left join (select cl.comment_id, count(*) as likes from "CommentLikes" cl group by cl.comment_id) as comment_likes on comment_likes.comment_id = c.id
                       		group by author) as comment_likes
                       on post_likes.author = comment_likes.author
-                      left join "Users" u2 on u2.id = post_likes.author';
+                      left join "Users" u2 on u2.id = post_likes.author
+                      order by score desc';
 
             $alltime_leaders = pg_query($db_connection, $stmt);
 
             while ($alltime_leader_row = pg_fetch_row($alltime_leaders)) {
               // print_r($alltime_leader_row);
-              $final_score = $alltime_leader_row[2] + $alltime_leader_row[3];
-              if (!$final_score) continue;
+              if ($alltime_leader_row[4]<1) continue;
               echo "<tr>";
               echo "<td>".$alltime_leader_row[0]."</td>";
               echo "<td>".$alltime_leader_row[2]."</td>";
               echo "<td>".$alltime_leader_row[3]."</td>";
-              echo "<td>".$final_score."</td>";
+              echo "<td>".$alltime_leader_row[4]."</td>";
               echo "</tr>";
             }
 

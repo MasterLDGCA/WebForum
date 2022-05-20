0d7d7eb545096563db142c5a7d8da5ccbc8aa923
Charith <charithakalanka@live.com>
2022-05-21 04:21:24 +1000

    Formpost layout fix

--- web/formpost.php
+++ web/formpost.php
@@ -15,7 +15,7 @@ requireLogin();
 
 $errors = null;
 
- //print_r($_POST);
+//print_r($_POST);
 
 ?>
 <div class="content">
@@ -28,24 +28,30 @@ $errors = null;
    	 ?>
 
 	<form method="post" action="index.php">
-	<label for="subjects">Subject of your discussion posts</label>
-	<select name="subjects" id="subjects" required = "required" >
-	<?php
-		$stmt = 'select * from "Subjects"';
-		 $sql = pg_query($db_connection, $stmt);
-		 while ($row = pg_fetch_assoc($sql)) {
-			 print_r($row);
-			 echo '<option value="'.htmlspecialchars($row["id"]).'">'.$row["title"].'</option>';
-		 }
-	?>
-	</select>
-		</br>
-		<label for="issue">Post your discussion here:</label>
-	  	<input type="text" class="title" name="title" placeholder="your discussion topic" value="<?php echo (isset($_POST['title'])) ? autofocus : "" ?>" required = "required">
-	  	<input style="height:200px; width: 600px; font-size:10pt;" type="text" size="500" id="content" name="content" placeholder="Write your answer of enquiry here..." value="<?php echo (isset($_POST['content'])) ? autofocus: "" ?>" required = "required"></br>
-
-		<button type= "submit">Submit</button>
-		<input type= "reset" value="Reset Form"/>
-
+		<div class="form-box">
+			<div class="form-group">
+				<label for="subjects">Subject of your discussion : </label>
+				<select name="subjects" id="subjects" required = "required">
+					<option value="1" selected="selected">General</option>
+					<?php
+						$stmt = 'select * from "Subjects"';
+					 	$sql = pg_query($db_connection, $stmt);
+					 	while ($row = pg_fetch_assoc($sql)) :?>
+					 		<?php if ($row["id"] == 1) continue; ?>
+						 	<option value="<?php echo htmlspecialchars($row["id"]); ?>"><?php echo $row["title"] ?></option>;
+					 <?php endwhile; ?>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="issue">Title of your discussion : </label><input type="text" name="title" placeholder="your discussion topic" value="<?php echo (isset($_POST['title'])) ? autofocus : "" ?>" required = "required">
+			</div>
+			<div class="form-group">
+				<input style="height:200px; width: 600px; font-size:10pt;" type="text" size="500" id="content" name="content" placeholder="Write your answer of enquiry here..." value="<?php echo (isset($_POST['content'])) ? autofocus: "" ?>" required = "required">
+			</div>
+			<div class="form-group">
+				<button type= "submit">Submit</button>
+				<input type= "reset" value="Reset Form"/>
+			</div>
+		</div>
 	</form>
 </div>
